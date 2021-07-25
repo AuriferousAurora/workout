1314f2ac1a22d9b743fd33deb457ec76c12b1d30
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2021-07-25 17:47:52 -0500

    Completed a rough outline of the add exercise instance subsection of editWorkout componenet. Next step is to display all pending exercise instances for a workout and then figure out how to send an array of exercise instances to the backend and put them in the database.

--- client/src/components/EditWorkout.js
+++ client/src/components/EditWorkout.js
@@ -5,15 +5,59 @@ import { ExerciseContext } from '../context/ExerciseContext';
 import { baseURL } from '../globals';
 import useFetch from  '../hooks/useFetch';
 
-import AddExerciseInstance from './AddExerciseInstance';
-
 export default function EditWorkout() {
     const { id } = useParams();
 
-    return (
-        <div>{id} edit
-             <AddExerciseInstance />
+    const [search, setSearch ] = useState([]);
+    const [sets, setSets] = useState(0);
+    const [reps, setReps] = useState(0);
+    const [exercisesInstances, setExerciseInstances] = useState([]);
+
+    const { exercises, setExercises } = useContext(ExerciseContext);
+    const { data } = useFetch(baseURL + 'exercises');
+
+    useEffect(() => {
+        if (data) setExercises(data.data.exercises);
+    }, [data])
+
+    const filterdExercises = search.length === 0 ? exercises :
+        exercises.filter(exercise => exercise.name.toLowerCase().includes(search.toLowerCase()))
+
+    const addExerciseInstance = () => {
+        let exercise;
+        exercises.forEach( i => {
+            if (i.name === search) exercise = i;
+        });
+        console.log(exercise);
+        if (exercise) {
+            console.log(exercise.name, sets, reps);
+        } else {
+            console.log('boo');
+        }
+    }
+
+    return  (
+        <div className='add_exercise_instance'>
+            <input type="text" 
+                placeholder="select exercise" 
+                value={search}
+                onChange={(e) => setSearch(e.target.value)} />
+            <input type="text" 
+                placeholder="select num sets" 
+                value={sets}
+                onChange={(e) => setSets(e.target.value)} />
+            <input type="text" 
+                placeholder="select num reps" 
+                value={reps}
+                onChange={(e) => setReps(e.target.value)} />
+            <button onClick={e => addExerciseInstance(e.target.value)}>Add</button>
+            <div>
+                {filterdExercises.map(exercise => {
+                    return (
+                        <div key={exercise.id}>{exercise.name}</div>
+                    )
+                })}
+            </div>
         </div>
-   
-    );
+    )
 }
